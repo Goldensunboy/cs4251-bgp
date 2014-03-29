6666e06eed480e8e83565114647cf38d9ed4d555
Ilyssa Widen <iwiden3@gmail.com>
2014-03-29 16:23:53 -0400

    ASNode Class

---
+++ src/ASNode.java
@@ -0,0 +1,42 @@
+import java.util.Map;
+import java.util.ArrayList;
+
+public class ASNode {
+	int ASNum;
+	Map<Integer, ArrayList<ASNode>> paths;
+
+	public ASNode(int ASNum, Map<Integer, ArrayList<ASNode>> paths) {
+		this.ASNum = ASNum;
+		this.paths = paths;
+	}
+
+	public void announce(ArrayList<ASNode> paths){
+		
+	}
+	
+	
+	
+	
+	
+	public int getASNum() {
+		return ASNum;
+	}
+
+	public void setASNum(int aSNum) {
+		ASNum = aSNum;
+	}
+
+	public Map<Integer, ArrayList<ASNode>> getPaths() {
+		return paths;
+	}
+
+	public void setPaths(Map<Integer, ArrayList<ASNode>> paths) {
+		this.paths = paths;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
